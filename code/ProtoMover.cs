53b4d94ea63fc157becba0167a0da06116ab6673
Tatu Riikonen <tatu.riikonen@tuni.fi>
2025-01-29 14:33:02 +0200

    modified ProtoMover.cs

--- code/ProtoMover.cs
+++ code/ProtoMover.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class ProtoMover : Node2D
 {
-	[Export] public float _speed = 100.0f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
